6e2e7c740548b6376f0bf6ca4bcc820496060870
林岚 <72348612+zenghao-1@users.noreply.github.com>
2023-04-13 21:41:52 +0800

    Add files via upload

---
+++ FileName.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+class B 
+{
+public:
+	B(){}
+	B(int i, int j)
+	{
+		x = i; y = j;
+	}
+	void printb()
+	{
+		cout << x << "," << y << endl;
+	}
+
+
+private:
+	int x, y;
+};
+
+class A
+{
+public:
+	A(){}
+	A(int I, int j);
+	void printa();
+private:
+	B c;
+};
+
+A::A(int i,int j):c(i,j)
+{}
+void A::printa()
+{
+	c.printb();
+}
+
+int main()
+{
+	A a(7, 8);
+	a.printa();	
+
+
+}
+
+
